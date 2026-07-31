7568d99c9fade226ed10a827cb2c768262f89a70
heet2-9 <heetgondalia9@gmail.com>
2026-07-31 22:10:07 +0530

    watermark

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -29,7 +29,7 @@ export default function Home() {
       {/* Watermark */}
       <div className="fixed bottom-3 right-4 z-50 pointer-events-none">
         <span className="text-[10px] text-text-muted/40 font-sans tracking-wide">
-          anu.builds.in
+
         </span>
       </div>
 
